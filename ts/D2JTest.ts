825fd8110a487a645a346e0c8b7e71844ace6835
Yusuke NAKAYAMA <santa@ubuntu.(none)>
2013-06-18 13:05:31 +0900

    Change D2JTest.ts

--- ts/D2JTest.ts
+++ ts/D2JTest.ts
@@ -1,9 +1,26 @@
 import DCaseTree = module("DCaseTree");
 
 var root : DCaseTree.DCaseNode = new DCaseTree.GoalNode("", "hogehoge", 1);
-var child : DCaseTree.DCaseNode = new DCaseTree.StrategyNode("", "hogehoge", 1);
+var child1 : DCaseTree.DCaseNode = new DCaseTree.StrategyNode("", "hogehoge", 1);
+var child2 : DCaseTree.DCaseNode = new DCaseTree.StrategyNode("", "hogehoge", 2);
+var child3: DCaseTree.DCaseNode = new DCaseTree.GoalNode("", "hogehoge", 3);
+var child4: DCaseTree.DCaseNode = new DCaseTree.GoalNode("", "hogehoge", 4);
+var child5: DCaseTree.DCaseNode = new DCaseTree.StrategyNode("", "hogehoge", 5);
+var child6: DCaseTree.DCaseNode = new DCaseTree.StrategyNode("", "hogehoge", 6);
+var child7: DCaseTree.DCaseNode = new DCaseTree.GoalNode("", "hogehoge", 7);
+var child8: DCaseTree.DCaseNode = new DCaseTree.GoalNode("", "hogehoge", 8);
 
-root.Children.push(child);
 
-var testJson : any[] = [];
-console.log(root.convertAllChildNodeIntoJson(testJson));
+root.Children.push(child1);
+root.Children.push(child2);
+
+child1.Children.push(child3);
+child2.Children.push(child4);
+
+child4.Children.push(child5);
+child4.Children.push(child6);
+
+child5.Children.push(child7);
+child6.Children.push(child8);
+
+console.log(root.convertAllChildNodeIntoJson([]));
